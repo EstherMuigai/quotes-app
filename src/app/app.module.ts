53ea8be67865c187774180d6c8bb03c4139199f0
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-03-22 17:21:56 +0300

    add project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainQuoteComponent } from './main-quote/main-quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainQuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    HighlightQuoteDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
